397049a487d349bd90bea01d13583ff397b0aa1e
Satyendra Sharma <satyendra.sharma@oracle.com>
2018-09-28 19:15:32 +0530

    minor code cleanup

--- process.py
+++ process.py
@@ -9,7 +9,7 @@ p = Path('').resolve()
 
 
 def configure_section_mapping():
-    """Read the configuration Excel file and cache the question/answer objects."""
+    """Reads the configuration Excel file and caches the question/answer objects."""
     section_map = ExcelReader('resources\\section_mapping.xlsx', max_col=6)
     for mapping in section_map.fetch_all_rows():
         ApplicationFactory.create_question_answer_mapping(mapping)
@@ -17,7 +17,7 @@ def configure_section_mapping():
 
 
 def process_data_input():
-    """Read the input Excel file and generate the application-data objects."""
+    """Reads the input Excel file and generates application-data objects."""
     er = ExcelReader('resources\\data_input.xlsx')
     for app_entry in er.fetch_all_rows():
         ApplicationFactory.create_app_data(app_entry)
@@ -35,15 +35,19 @@ def process_applications():
     for app_name, app_data in ApplicationFactory.app_cache.items():
         print('Processing [' + app_name + ']')
         child_doc = mdh.create_child_document()
-        # TODO Process the content of this file
-        print('Creating document for application : ' + app_name)
+        print('Creating child document for application : ' + app_name)
         paragraphs = (para for para in child_doc.paragraphs)
 
         for q_value, question in Question.cache_value.items():
-            # TODO PERFORMANCE : Process the entire document for each question.
-            if question.q_type == QuestionType.LARGE_TEXT:
-                answer = app_data.get_answers(question)
+            # PERFORMANCE ISSUE: Processing the entire document for each question.
+            q_type = question.q_type
+            answer = app_data.get_answers(question)
+            if q_type == QuestionType.LARGE_TEXT:
                 process_large_text_question(question, answer, paragraphs)
+            elif q_type == QuestionType.CHECKBOX:
+                process_checkbox_question(question, answer, paragraphs)
+            elif q_type == QuestionType.RADIO:
+                process_radio_question(question, answer, paragraphs)
 
         MasterDocumentHandler.save(app_name, child_doc)
         # break
@@ -54,13 +58,23 @@ def process_large_text_question(question, answer, paragraphs):
     for para in paragraphs:
         if q_tag in para.text:
             para.text = para.text.replace(q_tag, answer)
-        print('#' + para.text)
+        print('[L}' + para.text)
 
 
-def process_checkbox_question():
+def process_checkbox_question(question, answers, paragraphs):
+    q_tag = question.q_tag
+    for para in paragraphs:
+        if q_tag in para.text:
+            para.text = para.text.replace(q_tag, answers)
+        print('[C}' + para.text)
     pass
 
 
-def process_radio_question():
+def process_radio_question(question, answer, paragraphs):
+    q_tag = question.q_tag
+    for para in paragraphs:
+        if q_tag in para.text:
+            para.text = para.text.replace(q_tag, answer)
+        print('[R}' + para.text)
     pass
 
